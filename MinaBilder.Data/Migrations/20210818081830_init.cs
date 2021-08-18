522983841f363abf658399619f81a06057373d76
Pär Sjöberg <paer.sjoberg@googlemail.com>
2021-08-18 10:25:34 +0200

    Lagt till stöd för EF-migrations

---
+++ MinaBilder.Data/Migrations/20210818081830_init.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MinaBilder.Data.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Album",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Namn = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Album", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Filer",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Namn = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Storlek = table.Column<long>(type: "bigint", nullable: false),
+                    Sökväg = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Filer", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AlbumFil",
+                columns: table => new
+                {
+                    AlbumId = table.Column<int>(type: "int", nullable: false),
+                    FilerId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AlbumFil", x => new { x.AlbumId, x.FilerId });
+                    table.ForeignKey(
+                        name: "FK_AlbumFil_Album_AlbumId",
+                        column: x => x.AlbumId,
+                        principalTable: "Album",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AlbumFil_Filer_FilerId",
+                        column: x => x.FilerId,
+                        principalTable: "Filer",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AlbumFil_FilerId",
+                table: "AlbumFil",
+                column: "FilerId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AlbumFil");
+
+            migrationBuilder.DropTable(
+                name: "Album");
+
+            migrationBuilder.DropTable(
+                name: "Filer");
+        }
+    }
+}
